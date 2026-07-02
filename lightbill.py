e9d6b97952fe4c1aab2414074dbcab427cde84cf
Jayati Bhatt <jayatibhatt54@gmail.com>
2026-07-02 22:50:06 +0530

    lightbill.py

--- lightbill.py
+++ lightbill.py
@@ -3,33 +3,16 @@
 Light_Bill
 """
 
-while True:
-    print("\n===== ELECTRICITY BILL MENU =====")
-    print("1. Calculate Electricity Bill")
-    print("2. Exit")
+units = int(input("Enter electricity units: "))
 
-    choice = int(input("Enter your choice: "))
+if units <= 100:
+    bill = units * 2
+elif units <= 200:
+    bill = units * 3
+elif units <= 300:
+    bill = units * 5
+else:
+    bill = units * 7
 
-    if choice == 1:
-        units = int(input("Enter units consumed: "))
-
-        if units <= 100:
-            bill = units * 2
-
-        elif units <= 200:
-            bill = (100 * 2) + (units - 100) * 3
-
-        elif units <= 300:
-            bill = (100 * 2) + (100 * 3) + (units - 200) * 5
-
-        else:
-            bill = (100 * 2) + (100 * 3) + (100 * 5) + (units - 300) * 7
-
-        print("Total Electricity Bill = ₹", bill)
-
-    elif choice == 2:
-        print("Exit......")
-        break
-
-    else:
+print("Electricity Bill = ₹", bill)
         print("Invalid Choice! Please try again.")
